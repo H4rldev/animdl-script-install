f4f223c6c516accf4c52975481f11650dc6cacff
H4rl <0derp05@gmail.com>
2022-05-12 13:33:52 +0200

    mogus

--- include/chocoinstall.bat
+++ include/chocoinstall.bat
@@ -1,2 +1,12 @@
+if exist "%ALLUSERSPROFILE%\chocolatey\bin" (
+   echo Already installed continuing.
+   goto amongus
+) else (
+   goto install
+)
+
+:install
 ::This installs choco, as found on https://chocolatey.org/docs/installation
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+:amongus
