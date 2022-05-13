ab0f69e8fb11dd1fdfb9743b8c8d8f2084c55105
H4rl <0derp05@gmail.com>
2022-05-13 09:31:30 +0200

    mogus

--- Downloader.bat
+++ Downloader.bat
@@ -4,9 +4,9 @@ title Anim-DL Downloader :)
 
 :checkforinstallfiles
 if exist "%~dp0\include" "%~dp0\install.bat" (
-   del /F /Q %~dp0\include\*
+   del /F /Q "%~dp0\include\*"
    if exist "%~dp0\install.bat" (
-      del /F /Q %~dp0\install.bat
+      del /F /Q "%~dp0\install.bat"
       goto init
    ) else (
       goto init
