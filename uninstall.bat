f74378a92c48fe7d8745ee9890621c0317437cf6
H4rl <0derp05@gmail.com>
2022-05-27 00:31:07 +0200

    yes, depression hitting hard fr rn

--- uninstall.bat
+++ uninstall.bat
@@ -17,8 +17,29 @@ type %~dp0\logo.txt
 type %~dp0\credits.txt
 echo animdl and the batch script should now be gone. Goodbye
 cls
-del /F /Q logo.txt
-del /F /Q credits.txt
+del /F /Q %~dp0\logo.txt
+del /F /Q %~dp0\credits.txt
+del /F /Q %~dp0\license.txt
+del /F /Q %~dp0\Downloader.bat
+if exist "%~dp0\update.bat" (
+   del /F /Q %~dp0\update.bat
+   goto uninstall3
+) else (
+   goto check1
+)
+
+:check1
+if exist "%USERPROFILE%\.animdl" (
+   RD /S /Q %USERPROFILE%\.animdl
+   goto uninstall3
+) else (
+   goto uninstall3
+)
+
+:uninstall3
+echo I'm sorry to see you go.
+echo I hope you enjoyed using this tool.
+echo Bye.
 pause
-start "yes" "cmd /K del /F /Q %~dp0\uninstall.bat && exit"
+start /B "yes" "cmd /C del /F /Q %~dp0\uninstall.bat && exit"
 exit
